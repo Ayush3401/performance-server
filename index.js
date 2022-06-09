e7869524e3ea5a3b7dcc373f5022368cdb64a007
Ayush Kumar Gupta <ayush.gupta@sprinklr.com>
2022-06-09 21:19:12 +0530

    Add headers to requests

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const cors =require('cors');
 const app = express();
 app.use(cors());
 const {getAudits} = require('./lighthouse')
-const PORT = 8000;
+const PORT = 8080;
 
 app.get("/", async (req, res) => {
   const url = req.query.url
-  const audits = await getAudits(url)
-  res.send(audits)
+  console.log(JSON.parse(req.query.headers))
+  const audits = await getAudits(url, JSON.parse(req.query.headers))
+  if(audits!=={}) res.send(audits)
+  else res.status(500).send(audits)
 });
 
 app.listen(PORT, () => {
--- lighthouse.js
+++ lighthouse.js
@@ -1,30 +1,36 @@
-const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
 
-const getAudits = async (url) => {
-  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless', '--disable-gpu']});
-  const options = {onlyAudits: [
-    'resource-summary',
-    'mainthread-work-breakdown',
-    'bootup-time',
-    'third-party-summary',
-    'network-requests',
-    'network-rtt',
-    'network-server-latency',
-  ], port: chrome.port};
-  try{
+const getAudits = async (url, headers) => {
+  const chrome = await chromeLauncher.launch({
+    chromeFlags: ["--headless", "--disable-gpu"],
+  });
+  const options = {
+    logLevel: 'info',
+    onlyAudits: [
+      "resource-summary",
+      "mainthread-work-breakdown",
+      "bootup-time",
+      "third-party-summary",
+      "network-requests",
+      "network-rtt",
+      "network-server-latency",
+    ],
+    port: chrome.port,
+    extraHeaders: headers,
+  };
+  try {
     const runnerResult = await lighthouse(url, options);
-    const audits = runnerResult.lhr.audits
+    const audits = runnerResult.lhr.audits;
     await chrome.kill();
-    return audits
-  }
-  catch(err){
-    console.error(err)
+    return audits;
+  } catch (err) {
+    console.error(err);
     await chrome.kill();
-    return {}
+    return {};
   }
-}
+};
 
 module.exports = {
   getAudits: getAudits,
-}
+};
